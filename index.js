7b36074f9c24f40d5b8ba6a10d13a6f7cfcf9a82
Alexandra Hargrove <alexandrahargrove@live.com>
2021-02-19 18:35:20 -0800

    End of Part 1 of Part 2

---
+++ index.js
@@ -0,0 +1,31 @@
+const { client } = require('./db');
+client.connect();
+
+const PORT = 3000;
+const express = require('express');
+const server = express();
+const apiRouter = require('./api');
+
+
+const bodyParser = require('body-parser');
+server.use(bodyParser.json());
+
+const morgan = require('morgan');
+server.use(morgan('dev'));
+
+server.use('/api', apiRouter);
+
+
+server.use((req, res, next) => {
+    console.log("<____Body Logger START____>");
+    console.log(req.body);
+    console.log("<_____Body Logger END_____>");
+  
+    next();
+  });
+
+  
+
+server.listen(PORT , () => {
+    console.log('The server us up on port', PORT)
+});
