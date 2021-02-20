6f57484bb708c6ab0f85c1712a4a0870d2ad9669
Alexandra Hargrove <alexandrahargrove@live.com>
2021-02-20 11:10:20 -0800

    End of Part 2 in Part 2

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
+const apiRouter = require('./api');
 const { client } = require('./db');
 client.connect();
-
 const PORT = 3000;
 const express = require('express');
 const server = express();
-const apiRouter = require('./api');
+
 
 
 const bodyParser = require('body-parser');
